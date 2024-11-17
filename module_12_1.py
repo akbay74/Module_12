a16a0ef8edae9a006b02cbfe1fa020d00b6e6890
akbay74 <asv1974@list.ru>
2024-11-17 17:08:55 +0400

    Update module_12_1.py

--- module_12_1.py
+++ module_12_1.py
@@ -4,18 +4,23 @@ from runner import Runner
 
 class RunnerTest(unittest.TestCase):
 
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         t_walk = Runner('Ivan')
         for _ in range(10):
             t_walk.walk()
         self.assertEqual(t_walk.distance, 50)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         t_run = Runner('Pavel')
         for _ in range(10):
             t_run.run()
         self.assertEqual(t_run.distance, 100)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         t_walk = Runner('Ivan')
         t_run = Runner('Pavel')
